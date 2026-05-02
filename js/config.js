bb57d841c6c741d9c67f482956d3efc61c2ec670
Akshata Sardeshpande <35599717+akshatasardeshpande@users.noreply.github.com>
2026-05-02 13:18:06 -0500

    Update configuration for Cognito user pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_IcKYYoCau', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2f2juqaa0jadkbkj0mp6p7a3bo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
