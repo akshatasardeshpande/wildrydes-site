7e2d177546fc5ce573676bbfa0e33a7cb38ff8de
Akshata Sardeshpande <35599717+akshatasardeshpande@users.noreply.github.com>
2026-05-02 15:35:18 -0500

    Update API invoke URL in config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://main.d16azbqaze0fph.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://a9li8auljh.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
