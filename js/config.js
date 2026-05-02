035b0c4a3c0992be570009698d265f37d60e68de
Akshata Sardeshpande <35599717+akshatasardeshpande@users.noreply.github.com>
2026-05-02 13:33:55 -0500

    Change region in config to 'us-east-1'
    Updated the region in the configuration to 'us-east-1'.

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_IcKYYoCau', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2f2juqaa0jadkbkj0mp6p7a3bo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
